b028817127842df5f5e0352a43eaadb82b366670
PTuan <162488223+Tuan09032005@users.noreply.github.com>
2025-11-06 00:11:43 +0000

    testcommit

--- src/Layout.js
+++ src/Layout.js
@@ -15,7 +15,7 @@ const Layout = () => {
                 <ul className="menutrai" style={{ width: "250px" }}>
                   <li>
                     <a href="/" class="menutrai">
-                      TRANG CHU
+                      TRANG CHUkkk
                     </a>
                   </li>
                   <li>
